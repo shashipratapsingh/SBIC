9ae71058bdade1c923682d297065b1f3bec58f44
shashipratapsingh <itpro8577@gmail.com>
2025-03-26 18:58:45 +0530

    testing git cmd

--- src/main/java/com/SBICARD/SBICardApplication.java
+++ src/main/java/com/SBICARD/SBICardApplication.java
@@ -12,6 +12,8 @@ public class SBICardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SBICardApplication.class, args);
 		System.out.println("Started Successfully");
+
+		System.out.println("Testing");
 	}
 
 }
